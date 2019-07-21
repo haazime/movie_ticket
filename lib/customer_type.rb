dab8f884d5538da55031754a8d16f0ea1b09354e
haazime <h4zime@gmail.com>
2019-07-21 18:28:39 +0900

    student, high and junior high school student

--- lib/customer_type.rb
+++ lib/customer_type.rb
@@ -6,4 +6,6 @@ module CustomerType
   CinemaCitizen = CinemaCitizenClass.new(:cinema_citizen)
   CinemaCitizenOver60 = Base.new(:cinema_citizen_over60)
   Senior = Base.new(:senior)
+  Student = Base.new(:student)
+  HighAndJuniorHighSchoolStudent = Base.new(:high_and_junior_high_school_student)
 end
--- spec/fee_rule_spec.rb
+++ spec/fee_rule_spec.rb
@@ -223,4 +223,112 @@ describe '料金ルール' do
       it { is_expected.to eq(Fee.new(CustomerType::Senior, DayType::DayOfCinema, nil)) }
     end
   end
+
+  context '学生' do
+    let(:customer_type) { CustomerType::Student }
+
+    context '平日' do
+      context '~20:00' do
+        let(:at) { ScreeningAt.parse('2019-07-10 15:30') }
+        it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::Weekday, ShowType::Normal)) }
+      end
+
+      context '20:00~' do
+        let(:at) { ScreeningAt.parse('2019-07-10 20:15') }
+
+        context '通常上映' do
+          it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::Weekday, ShowType::Late)) }
+        end
+
+        context '爆音上映' do
+          let(:sound_type) { SoundType::Explosion }
+          it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::Weekday, ShowType::Normal)) }
+        end
+      end
+    end
+
+    context '土日祝' do
+      context '~20:00' do
+        let(:at) { ScreeningAt.parse('2019-07-13 15:30') }
+        it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::Holiday, ShowType::Normal)) }
+      end
+
+      context '20:00~' do
+        let(:at) { ScreeningAt.parse('2019-07-13 20:15') }
+
+        context '通常上映' do
+          it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::Holiday, ShowType::Late)) }
+        end
+
+        context '爆音上映' do
+          let(:sound_type) { SoundType::Explosion }
+          it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::Holiday, ShowType::Normal)) }
+        end
+      end
+    end
+
+    context '映画の日（平日）' do
+      let(:at) { ScreeningAt.parse('2019-07-01 15:30') }
+      it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::DayOfCinema, nil)) }
+    end
+
+    context '映画の日（土日祝）' do
+      let(:at) { ScreeningAt.parse('2019-06-01 15:30') }
+      it { is_expected.to eq(Fee.new(CustomerType::Student, DayType::DayOfCinema, nil)) }
+    end
+  end
+
+  context '中高生' do
+    let(:customer_type) { CustomerType::HighAndJuniorHighSchoolStudent }
+
+    context '平日' do
+      context '~20:00' do
+        let(:at) { ScreeningAt.parse('2019-07-10 15:30') }
+        it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::Weekday, ShowType::Normal)) }
+      end
+
+      context '20:00~' do
+        let(:at) { ScreeningAt.parse('2019-07-10 20:15') }
+
+        context '通常上映' do
+          it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::Weekday, ShowType::Late)) }
+        end
+
+        context '爆音上映' do
+          let(:sound_type) { SoundType::Explosion }
+          it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::Weekday, ShowType::Normal)) }
+        end
+      end
+    end
+
+    context '土日祝' do
+      context '~20:00' do
+        let(:at) { ScreeningAt.parse('2019-07-13 15:30') }
+        it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::Holiday, ShowType::Normal)) }
+      end
+
+      context '20:00~' do
+        let(:at) { ScreeningAt.parse('2019-07-13 20:15') }
+
+        context '通常上映' do
+          it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::Holiday, ShowType::Late)) }
+        end
+
+        context '爆音上映' do
+          let(:sound_type) { SoundType::Explosion }
+          it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::Holiday, ShowType::Normal)) }
+        end
+      end
+    end
+
+    context '映画の日（平日）' do
+      let(:at) { ScreeningAt.parse('2019-07-01 15:30') }
+      it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::DayOfCinema, nil)) }
+    end
+
+    context '映画の日（土日祝）' do
+      let(:at) { ScreeningAt.parse('2019-06-01 15:30') }
+      it { is_expected.to eq(Fee.new(CustomerType::HighAndJuniorHighSchoolStudent, DayType::DayOfCinema, nil)) }
+    end
+  end
 end
